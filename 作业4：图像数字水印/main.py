654e23bc8fce195190f2804567694e6310968185
Freeman449s <961735889@qq.com>
2020-10-26 22:39:41 +0800

    修改main脚本，上传测试图

--- 作业4：图像数字水印/main.py
+++ 作业4：图像数字水印/main.py
@@ -1,16 +1,9 @@
-# 这是一个示例 Python 脚本。
+from PIL import Image
+from PIL.Image import ANTIALIAS
+import numpy as np
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 Double Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
-
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
-
-
-# 按间距中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+srcImg = Image.open("Source.jpg")
+srcM = np.asarray(srcImg)
+wmImg = Image.open("Watermark.jpg")
+wmImg.resize(srcImg.size, ANTIALIAS)  # 将水印图尺寸调整到与原图一致
+wmM = np.asarray(wmImg)
