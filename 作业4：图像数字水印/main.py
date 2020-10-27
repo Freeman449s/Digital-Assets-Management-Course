eca823399db94a0d5c9b8953ac04985f16ddc88f
Freeman449s <961735889@qq.com>
2020-10-27 20:15:57 +0800

    完成后端处理，添加测试图片

--- 作业4：图像数字水印/main.py
+++ 作业4：图像数字水印/main.py
@@ -2,12 +2,16 @@ from PIL import Image
 from PIL.Image import ANTIALIAS
 import numpy as np
 
+originalSize = (3072, 2048)  # 水印图的原始尺寸
+
 
 def addWatermark(srcPath, markPath):
+    global originalSize
     srcImg = Image.open(srcPath)
     srcM = np.array(srcImg)  # 尺寸为height * width * channels
     srcM.flags.writeable = True
     markImg = Image.open(markPath)
+    originalSize = markImg.size
     markImg = markImg.resize(srcImg.size, ANTIALIAS)  # 将水印图尺寸调整到与原图一致
     markM = np.array(markImg)
     markM.flags.writeable = True
@@ -24,4 +28,21 @@ def addWatermark(srcPath, markPath):
     markedImg.save("Image with Watermark.bmp")
 
 
-addWatermark("Source.jpg", "Watermark.jpg")
+def extractWatermark(markedImgPath):
+    markedImg = Image.open(markedImgPath)
+    markedM = np.array(markedImg)
+    markedM.flags.writeable = True
+    # 提取每个像素的低2位，乘以85
+    for i in range(0, markedM.shape[0]):
+        for j in range(0, markedM.shape[1]):
+            for k in range(0, markedM.shape[2]):
+                tmp = markedM[i][j][k] >> 2
+                tmp = tmp << 2
+                markedM[i][j][k] = (markedM[i][j][k] - tmp) * 85
+    extractedImg = Image.fromarray(markedM)
+    extractedImg.resize(originalSize, ANTIALIAS)
+    extractedImg.save("Extracted Watermark.bmp")
+
+
+addWatermark("Source.bmp", "Watermark.bmp")
+extractWatermark("Image with Watermark.bmp")
