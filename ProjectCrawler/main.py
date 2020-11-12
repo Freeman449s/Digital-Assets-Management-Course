0397cf928ba896479b07e00c09e6b52ecc38c42e
Freeman449s <961735889@qq.com>
2020-11-12 08:50:52 +0800

    代码清理

--- ProjectCrawler/main.py
+++ ProjectCrawler/main.py
@@ -1,14 +1,6 @@
 from PeopleInfo import PeopleInfo
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'}
-# 解析位置函数
-APIKEY = "VNUBZ-K252F-2U4JO-NSF35-SKNJO-UEB37"
-encoding = "UTF-8"
-
 nameListPath = "人物列表\\诗（词）人列表 - 后期处理.txt"
-peopleFilePath = "PeopleInfo.json"
-workFilePath = "WorkInfo.json"
 
 
 def main():
